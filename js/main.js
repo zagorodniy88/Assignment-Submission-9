cfd372135ef1f97c4f1374e81e7f515faf2c391d
Alex <zagorodniy88@gmail.com>
2021-10-05 12:20:21 -0300

    Assignment Submission # 9

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function CountDown_Function() {
+    let seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds -= 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+        }
+    }
+
+    tick();
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous btn
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
